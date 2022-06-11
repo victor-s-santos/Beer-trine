e9232604ec0d227bc55aafe8e0dff3f6398c05a4
victor-s-santos <victor.santos.cd@gmail.com>
2022-06-11 18:20:57 -0300

    ADD:add the beer model relation and model objects

--- api/models.py
+++ api/models.py
@@ -1,3 +1,78 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class BeerSchool(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = "school"
+        verbose_name_plural = "schools"
+
+
+class BeerHop(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = "hop"
+        verbose_name_plural = "hops"
+
+
+class BeerFamily(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = "family"
+        verbose_name_plural = "families"
+
+
+class BeerYeast(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = "yeast"
+        verbose_name_plural = "yeasts"
+
+
+class BeerFiltering(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = "filterings"
+        verbose_name_plural = "filtrations"
+
+
+class BeerCereal(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = "cereal"
+        verbose_name_plural = "cereals"
+
+
+# To Do - use abstract models to avoid repetition in cereals, family, yeast, filtering
+
+
+class Beer(models.Model):
+    name = models.CharField(max_length=200)
+    water_percentage = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
+    school = models.ForeignKey(BeerSchool, on_delete=models.SET_NULL, null=True)
+    hops = models.ForeignKey(BeerHop, on_delete=models.SET_NULL, null=True)
+    family = models.ForeignKey(BeerFamily, on_delete=models.SET_NULL, null=True)
+    yeast = models.ForeignKey(BeerYeast, on_delete=models.SET_NULL, null=True)
+    filtering = models.ForeignKey(
+        BeerFiltering, on_delete=models.SET_NULL, null=True
+    )
+    cereals = models.ManyToManyField(BeerCereal)
+
+    class Meta:
+        verbose_name = "beer"
+        verbose_name_plural = "beers"
