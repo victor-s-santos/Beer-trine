df2a21f0fe95c5cdcc4bacc5ecc526155e0175bc
victor-s-santos <victor.santos.cd@gmail.com>
2022-06-18 18:41:07 -0300

    feature: add beerfamilylistcreateview

--- api/views.py
+++ api/views.py
@@ -1,8 +1,67 @@
+from email.policy import HTTP
+
 from rest_framework import generics, status
 from rest_framework.response import Response
-from rest_framework.serializers import Serializer
 
-class BeerListCreateView(generics.ListCreateAPIView):
-    
-    def get(self, request):
-        return Response("Python")
+from api.models import (
+    Beer,
+    BeerCereal,
+    BeerFamily,
+    BeerFiltering,
+    BeerHop,
+    BeerSchool,
+    BeerYeast,
+)
+from api.serializers import (
+    BeerCerealSerializer,
+    BeerFamilySerializer,
+    BeerFilteringSerializer,
+    BeerHopSerializer,
+    BeerSchoolSerializer,
+    BeerSerializer,
+    BeerYeastSerializer,
+)
+
+
+class BeerCerealListCreateView(generics.ListCreateAPIView):
+    def get(self, request: dict) -> Response:
+        queryset = BeerCereal.objects.all()
+        serializer_class = BeerCerealSerializer(queryset, many=True)
+        if len(serializer_class.data) == 0:
+            return Response(
+                "There are no BeerCereal recorded.", status=status.HTTP_200_OK
+            )
+        return Response(serializer_class.data, status=status.HTTP_200_OK)
+
+    def post(self, request: dict) -> Response:
+        serializer_class = BeerCerealSerializer(data=request.data)
+        if serializer_class.is_valid():
+            serializer_class.save()
+            return Response(
+                serializer_class.data, status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer_class.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class BeerFamilyListCreateView(generics.ListCreateAPIView):
+    def get(self, request: dict) -> Response:
+        queryset = BeerFamily.objects.all()
+        serializer_class = BeerFamilySerializer(queryset, many=True)
+        if len(serializer_class.data) == 0:
+            return Response(
+                "There are no BeerFamily recorded.", status=status.HTTP_200_OK
+            )
+        return Response(serializer_class.data, status=status.HTTP_200_OK)
+
+    def post(self, request: dict) -> Response:
+        serializer_class = BeerFamilySerializer(data=request.data)
+        if serializer_class.is_valid():
+            serializer_class.save()
+            return Response(
+                serializer_class.data, status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer_class.errors, status=status.HTTP_400_BAD_REQUEST
+        )
