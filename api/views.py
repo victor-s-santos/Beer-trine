9fe2529f43b81fad20d69290cfaea68b5ddd53be
victor-s-santos <victor.santos.cd@gmail.com>
2022-06-18 19:01:04 -0300

    feature: add beerschoollistcreateview and its endpoint

--- api/views.py
+++ api/views.py
@@ -111,3 +111,26 @@ class BeerHopListCreateView(generics.ListCreateAPIView):
         return Response(
             serializer_class.errors, status=status.HTTP_400_BAD_REQUEST
         )
+
+
+class BeerSchoolListCreateView(generics.ListCreateAPIView):
+    def get(self, request: dict) -> Response:
+        queryset = BeerSchool.objects.all()
+        serializer_class = BeerSchoolSerializer(queryset, many=True)
+        if len(serializer_class.data) == 0:
+            return Response(
+                "There are no BeerSchool recorded.",
+                status=status.HTTP_200_OK,
+            )
+        return Response(serializer_class.data, status=status.HTTP_200_OK)
+
+    def post(self, request: dict) -> Response:
+        serializer_class = BeerSchoolSerializer(data=request.data)
+        if serializer_class.is_valid():
+            serializer_class.save()
+            return Response(
+                serializer_class.data, status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer_class.errors, status=status.HTTP_400_BAD_REQUEST
+        )
